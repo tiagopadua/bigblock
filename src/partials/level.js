ffb270bc0f21cdf2685856e7fb4f6681b1354904
Tiago Padua <tiagopadua@Tiagos-Mac-Pro.local>
2015-09-05 18:31:59 -0300

    fixed sizes

--- src/partials/level.js
+++ src/partials/level.js
@@ -1,10 +1,10 @@
 function Level(player) {
     // Create ground
-    var geometry = new THREE.PlaneBufferGeometry(10, 10, 10, 10);
+    var geometry = new THREE.PlaneBufferGeometry(100, 100, 100, 100);
     // Texture
     var floorTexture = new THREE.ImageUtils.loadTexture('img/checkers.png');
     floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-    floorTexture.repeat.set(10, 10);
+    floorTexture.repeat.set(20, 20);
     // Material
     var floorMaterial = new THREE.MeshLambertMaterial({
         map: floorTexture,
@@ -16,19 +16,18 @@ function Level(player) {
     
     // Create lights
     this.lights = [
-        new THREE.SpotLight(0xff5555, 7, 10, Math.PI / 3),
-        new THREE.SpotLight(0xffff55, 7, 10, Math.PI / 3),
-        new THREE.SpotLight(0x55ff55, 7, 10, Math.PI / 3),
-        new THREE.SpotLight(0x5555ff, 7, 10, Math.PI / 3),
-        new THREE.AmbientLight(0x222211)
+        new THREE.SpotLight(0xff5555, 8, 100, Math.PI / 3),
+        new THREE.SpotLight(0xffff55, 8, 100, Math.PI / 3),
+        new THREE.SpotLight(0x55ff55, 8, 100, Math.PI / 3),
+        new THREE.SpotLight(0x5555ff, 8, 100, Math.PI / 3)
     ];
     
     window.light = this.lights[0];
 
-    this.lights[0].position.set(5,  2,  5);
-    this.lights[1].position.set(5,  2, -5);
-    this.lights[2].position.set(-5, 2, -5);
-    this.lights[3].position.set(-5, 2,  5);
+    this.lights[0].position.set( 50, 15,  50);
+    this.lights[1].position.set( 50, 15, -50);
+    this.lights[2].position.set(-50, 15, -50);
+    this.lights[3].position.set(-50, 15,  50);
 
     //this.lights[0].target = this.lights[1].target = this.lights[2].target = this.lights[3].target = player.mesh;
 
@@ -36,11 +35,11 @@ function Level(player) {
     this.lights[0].shadowMapHeight = this.lights[1].shadowMapHeight = this.lights[2].shadowMapHeight = this.lights[3].shadowMapHeight = 1024;
 
     this.lights[0].shadowCameraNear = this.lights[1].shadowCameraNear = this.lights[2].shadowCameraNear = this.lights[3].shadowCameraNear = 0.001;
-    this.lights[0].shadowCameraFar = this.lights[1].shadowCameraFar = this.lights[2].shadowCameraFar = this.lights[3].shadowCameraFar = 20;
+    this.lights[0].shadowCameraFar = this.lights[1].shadowCameraFar = this.lights[2].shadowCameraFar = this.lights[3].shadowCameraFar = 120;
     this.lights[0].shadowCameraFov = this.lights[1].shadowCameraFov = this.lights[2].shadowCameraFov = this.lights[3].shadowCameraFov = 90;
     
     this.lights[0].castShadow = this.lights[1].castShadow = this.lights[2].castShadow = this.lights[3].castShadow = true;
-    this.lights[0].shadowDarkness = this.lights[1].shadowDarkness = this.lights[2].shadowDarkness = this.lights[3].shadowDarkness = 0.5;
+    this.lights[0].shadowDarkness = this.lights[1].shadowDarkness = this.lights[2].shadowDarkness = this.lights[3].shadowDarkness = 0.8;
     
     //this.lights[0].shadowCameraVisible = this.lights[1].shadowCameraVisible = this.lights[2].shadowCameraVisible = this.lights[3].shadowCameraVisible = true;
 }
@@ -51,7 +50,7 @@ Level.prototype.addComponentsToScene = function(scene) {
     for (i = 0; i < this.lights.length; ++i) {
         scene.add(this.lights[i]);
     }
-    scene.fog = new THREE.FogExp2(0x000000, 0.12);
+    scene.fog = new THREE.FogExp2(0x000000, 0.02);
 };
 
 Level.prototype.animate = function(time, player) {
